35c3b4c06dfa8d6467dda49e2407e909e0760de3
Loren Meta <loren_meta@yahoo.com>
2022-10-14 20:41:28 +0200

    adding cards component into list of cards + style

--- src/app/news/news-list/news-list.component.ts
+++ src/app/news/news-list/news-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { News } from '../news-interface';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-news-list',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-list.component.css']
 })
 export class NewsListComponent implements OnInit {
+  news: News[] = [];
 
-  constructor() { }
+  constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
+    this.loadNews();
   }
 
+  private loadNews(): void {
+    this.newsService.getNews().subscribe((news: News[]) => this.news = news)
+  }
 }
