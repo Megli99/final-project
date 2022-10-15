ba63731cc5d02b36d11b33ba215a66602055db11
Gerti <onegerti@gmail.com>
2022-10-15 12:46:25 +0200

    kot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SdaHttpClientModule } from './data-layer/sda-be-mock.module';
+import { SdaHttpClient } from './data-layer/sda-be-mock.service';
 import { MatchesModule } from './matches/matches.module';
 import { NewsModule } from './news/news.module';
 import { TeamsModule } from './teams/teams.module';
@@ -20,7 +21,7 @@ import { TeamsModule } from './teams/teams.module';
     MatchesModule,
     SdaHttpClientModule
   ],
-  providers: [],
+  providers: [SdaHttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
