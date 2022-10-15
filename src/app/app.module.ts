85080f6b00ce7d63e36edb4a077386a11e347d2c
Loren Meta <loren_meta@yahoo.com>
2022-10-15 11:48:42 +0200

    dev done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SdaHttpClientModule } from './data-layer/sda-be-mock.module';
-import { MatchesModule } from './matches/matches.module';
+import { SdaHttpClient } from './data-layer/sda-be-mock.service';
+import { MatchesModule } from './matches/matches.module'; 
 import { NewsModule } from './news/news.module';
 import { TeamsModule } from './teams/teams.module';
 
@@ -18,9 +19,10 @@ import { TeamsModule } from './teams/teams.module';
     NewsModule,
     TeamsModule,
     MatchesModule,
+    MatchesModule,
     SdaHttpClientModule
   ],
-  providers: [],
+  providers: [SdaHttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
