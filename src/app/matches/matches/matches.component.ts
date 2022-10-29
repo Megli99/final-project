fc8a188af9b46c9383b076bc973623d2b705a7ea
Loren Meta <loren_meta@yahoo.com>
2022-10-29 15:48:19 +0200

    added matches

--- src/app/matches/matches/matches.component.ts
+++ src/app/matches/matches/matches.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Teams } from 'src/app/teams/teams-interface';
+import { TeamsService } from 'src/app/teams/teams.service';
 import { Matches } from '../matches-interface';
 import { MatchesService } from '../matches.service';
 @Component({
@@ -8,14 +10,19 @@ import { MatchesService } from '../matches.service';
 })
 export class MatchesComponent implements OnInit {
   matches: Matches[] = [];
-
-  constructor(private matchesService: MatchesService) { }
+  teams: Teams[] = []
+  constructor(private matchesService: MatchesService, private teamsService: TeamsService) { }
 
   ngOnInit(): void {
     this.loadMatches();
+    this.loadTeams()
   }
 
   private loadMatches(): void {
     this.matchesService.getMatches().subscribe((matches: Matches[]) => this.matches = matches)
+  }
+
+  private loadTeams(): void {
+    this.teamsService.getTeams().subscribe((teams: Teams[]) => this.teams = teams)
   }
 }
